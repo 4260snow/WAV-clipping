3c20ac50f487a7d44589066c8ac8a956bb487f90
4260snow <75683039+4260snow@users.noreply.github.com>
2023-09-03 00:51:02 +0300

    Update func.h
    Изменены названия функций в заголовочном файле

--- func.h
+++ func.h
@@ -5,7 +5,7 @@
 
 void read(WaveRIFF* , WaveFormat* , WaveData* , int16_t**, FILE*);
 void write(WaveRIFF, WaveFormat, WaveData, int16_t*, FILE*);
-void new_data(int16_t**, int);
-void nd(int16_t**, int, int);
+void clip(int16_t**, int);
+void unclip(int16_t**, int, int);
 
 #endif
